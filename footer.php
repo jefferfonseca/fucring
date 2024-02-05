13f64ffa3b11764fd5ced8cc3d97721592b5f75d
jefferfonseca <jefferfonseca@gmail.com>
2024-02-05 07:36:09 -0500

    backup

--- footer.php
+++ footer.php
@@ -13,34 +13,34 @@
                 <div class="enlaces">
                     <a href="ayuda.php">Solicitar Ayuda</a>
                     <a href="ayuda-legal.php">Ayuda Legal</a>
-                    <a href="">Politicas de Donación</a>
-                    <a href="">Políticas de protección de datos</a>
-                    <a href="">Documentos publicos</a>
+                    <a href="assets/politica-donaciones.pdf" target="_blank">Politicas de Donación</a>
+                    <a href="assets/politica-proteccion-datos.pdf"  target="_blank">Políticas de protección de datos</a>
+                    <!-- <a href="">Documentos publicos</a> -->
                 </div>
             </div>
             <div class="col-sm-12 col-md-4">
                 <h1><b>Contactanos:</b> </h1>
                 <div class="enlaces">
                     <div id="redes">
-                        <a href=""><i class='bx bxl-facebook'></i></a>
-                        <a href=""><i class='bx bxl-instagram'></i></a>
-                        <a href=""><i class='bx bxl-youtube'></i></i></a>
+                        <a href="https://www.facebook.com/fucring?mibextid=ZbWKwL"><i class='bx bxl-facebook'></i></a>
+                        <a href="https://www.instagram.com/celestialmariachi?igsh=MTRlMGx0NjczaDh5eA=="><i class='bx bxl-instagram'></i></a>
+                        <a href="https://www.youtube.com/@nelsonfonsecaysumariachice2175"><i class='bx bxl-youtube'></i></i></a>
                     </div>
-                    <a href="tel:3102339306"> <i class='bx bxl-whatsapp'></i>(+57) 310 233 9306</a>
-                    <a href="tel:3102339306"> <i class='bx bxl-whatsapp'></i>(+57) 310 313 9854</a>
+                    <a href="cel:3102339306"> <i class='bx bxl-whatsapp'></i>(+57) 310 233 9306</a>
+                    <a href="cel:3102339306"> <i class='bx bxl-whatsapp'></i>(+57) 310 313 9854</a>
                     <a href="mailto:fucringtunja@gmail.com"><i class='bx bxl-gmail'></i>fucringtunja@gmail.com</a>
                 </div>
 
                 <h3><b>Dirección</b></h3>
-                <p>Cra. 3B 16a- 22 | Patriotas
-                    Tunja, Boyaca, Colombia
+                <p>
+                    Cra. 3B 16a- 22 | Patriotas
+                    Tunja, Boyacá, Colombia
                 </p>
                 <iframe
                     src="https://www.google.com/maps/d/u/0/embed?mid=1TTWEAZnf4a_ahn0_2SDdXC2qiIjvjls&ehbc=2E312F"
                     width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade">
                 </iframe>
-
             </div>
             <div class="col-sm-12" id="derechos">
                 <h1 class="lema">Nacidos para Servir</h1>
